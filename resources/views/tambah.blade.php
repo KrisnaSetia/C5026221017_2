bf361be33f41a3211b13558a93596144c343531b
KrisnaSetia <147696945+KrisnaSetia@users.noreply.github.com>
2023-12-03 10:44:19 +0700

    Tugas pertemuan tanggal 30 Nop

--- resources/views/tambah.blade.php
+++ resources/views/tambah.blade.php
@@ -1,25 +1,41 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Tutorial Membuat CRUD Pada Laravel - www.malasngoding.com</title>
-</head>
-<body>
+@extends('master2')
+@section('judulhalaman','Data Pegawai')
+@section('konten')
 
 	<h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
 	<h3>Data Pegawai</h3>
 
-	<a href="/pegawai"> Kembali</a>
+	<a href="/pegawai" class="btn btn-success"> ← Kembali</a>
 
 	<br/>
 	<br/>
 
-	<form action="/pegawai/store" method="post">
+	<form action="/pegawai/store" method="post" class="form-horizontal">
 		{{ csrf_field() }}
-		Nama <input type="text" name="nama"> <br/>
-		Jabatan <input type="text" name="jabatan"> <br/>
-		Umur <input type="number" name="umur"> <br/>
-		Alamat <textarea name="alamat"></textarea> <br/>
-		<input type="submit" value="Simpan Data">
+        <div class="form-group row">
+            <label for="nama" class="col-xs-2 col-form-label mr-2">Nama</label>
+            <div class="col-xs-1 ml-3">
+            <input type="text" class="form-control" id="nama" name="nama">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="jabatan" class="col-xs-2 col-form-label mr-2">Jabatan</label>
+            <div class="col-xs-1 ml-1">
+            <input type="text" class="form-control" id="jabatan" name="jabatan">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="umur" class="col-xs-2 col-form-label mr-2">Umur</label>
+            <div class="col-xs-1 ml-3">
+            <input type="number" class="form-control" id="umur" name="umur">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="alamat" class="col-xs-2 col-form-label mr-2">Alamat</label>
+            <div class="col-xs-1 ml-2">
+            <textarea class="form-control" id="alamat" name="alamat" ></textarea>
+            </div>
+        </div>
+		<input type="submit" value="Simpan Data" class="btn btn-primary">
 	</form>
-</body>
-</html>
+@endsection

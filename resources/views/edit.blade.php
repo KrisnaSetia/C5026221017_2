0a8163cd2a4aefa8c8239b6651e9fbcf01e3c434
KrisnaSetia <147696945+KrisnaSetia@users.noreply.github.com>
2023-12-11 06:38:50 +0700

    Pertemuan 30 Nop

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -14,26 +14,26 @@
 	<form action="/pegawai/update" method="post" class="form-horizontal">
 		{{ csrf_field() }}
         <div class="form-group row">
-            <label for="nama" class="col-xs-3 col-form-label mr-2">Nama</label>
+            <label for="nama" class="col-sm-2 col-form-label">Nama</label>
             <input type="hidden" name="id" value="{{ $p->pegawai_id }}"> <br/>
-            <div class="col-xs-9 ml-3">
+            <div class="col-sm-6">
             <input type="text" required="required" id="nama"  name="nama" class="form-control" value="{{ $p->pegawai_nama }}">
             </div>
         </div>
         <div class="form-group row">
-            <label for="jabatan" class="col-xs-3 col-form-label mr-2">Jabatan</label>
+            <label for="jabatan" class="col-sm-2 col-form-label">Jabatan</label>
             <div class="col-sm-6">
             <input type="text" required="required" id="jabatan"  name="jabatan" class="form-control" value="{{ $p->pegawai_jabatan }}">
             </div>
         </div>
         <div class="form-group row">
-            <label for="umur" class="col-xs-4 col-form-label mr-3">Umur</label>
+            <label for="umur" class="col-sm-2 col-form-label">Umur</label>
             <div class="col-sm-6">
             <input type="number" required="required" id="umur"  name="umur" class="form-control" value="{{ $p->pegawai_umur }}">
             </div>
         </div>
         <div class="form-group row">
-            <label for="alamat" class="col-xs-3 col-form-label mr-2">Alamat</label>
+            <label for="alamat" class="col-sm-2 col-form-label">Alamat</label>
             <div class="col-sm-6">
             <textarea required="required" name="alamat" class="form-control">{{ $p->pegawai_alamat }}</textarea>
             </div>
